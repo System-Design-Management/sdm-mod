c4ef61ebb44e3028f8ea15a4944076554a0defe8
miyoshi207 <miyoshi-naoki207@g.ecc.u-tokyo.ac.jp>
2026-03-19 21:20:38 +0900

    GUIを修正

--- src/client/java/jp/ac/u_tokyo/sdm/extra_items/client/screen/TechnicalBookScreen.java
+++ src/client/java/jp/ac/u_tokyo/sdm/extra_items/client/screen/TechnicalBookScreen.java
@@ -3,7 +3,6 @@ package jp.ac.u_tokyo.sdm.extra_items.client.screen;
 import jp.ac.u_tokyo.sdm.extra_items.screen.TechnicalBookScreenHandler;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 
@@ -12,7 +11,8 @@ public final class TechnicalBookScreen extends HandledScreen<TechnicalBookScreen
     private static final Text FORMULA_TEXT = Text.translatable("screen.extra_items.technical_book.formula");
     private static final Text EXPLANATION_TEXT = Text.translatable("screen.extra_items.technical_book.explanation");
     private static final Text FOOTNOTE_TEXT = Text.translatable("screen.extra_items.technical_book.footnote");
-    private static final Text CLOSE_TEXT = Text.translatable("screen.extra_items.technical_book.close");
+    private static final Text LEFT_PAGE_TEXT = Text.translatable("screen.extra_items.technical_book.left_page");
+    private static final Text RIGHT_PAGE_TEXT = Text.translatable("screen.extra_items.technical_book.right_page");
     private static final int PANEL_WIDTH = 292;
     private static final int PANEL_HEIGHT = 180;
 
@@ -31,15 +31,6 @@ public final class TechnicalBookScreen extends HandledScreen<TechnicalBookScreen
     @Override
     protected void init() {
         super.init();
-
-        int buttonWidth = 80;
-        int buttonHeight = 20;
-        int buttonX = this.x + (this.backgroundWidth - buttonWidth) / 2;
-        int buttonY = this.y + this.backgroundHeight - 30;
-
-        this.addDrawableChild(ButtonWidget.builder(CLOSE_TEXT, button -> this.close())
-            .dimensions(buttonX, buttonY, buttonWidth, buttonHeight)
-            .build());
     }
 
     @Override
@@ -47,60 +38,98 @@ public final class TechnicalBookScreen extends HandledScreen<TechnicalBookScreen
         // TODO: ページ送りや章構成を追加する場合は、左右ページごとに操作を分ける。
         int titleWidth = this.textRenderer.getWidth(this.title);
         int subtitleWidth = this.textRenderer.getWidth(SUBTITLE_TEXT);
+        int pageWidth = (this.backgroundWidth - 40) / 2;
+        int leftPageX = 20;
+        int rightPageX = this.backgroundWidth / 2 + 4;
 
-        context.drawText(this.textRenderer, this.title, (this.backgroundWidth - titleWidth) / 2, 14, 0xFF3A2412, false);
-        context.drawText(this.textRenderer, SUBTITLE_TEXT, (this.backgroundWidth - subtitleWidth) / 2, 30, 0xFF70543F, false);
-        context.drawCenteredTextWithShadow(this.textRenderer, FORMULA_TEXT, this.backgroundWidth / 2, 70, 0xFF2B1D12);
+        context.drawText(this.textRenderer, this.title, (this.backgroundWidth - titleWidth) / 2, 18, 0xFF4A3221, false);
+        context.drawText(this.textRenderer, SUBTITLE_TEXT, (this.backgroundWidth - subtitleWidth) / 2, 34, 0xFF8C6F58, false);
+        context.drawText(this.textRenderer, Text.literal("01"), leftPageX, 54, 0xFFAE9A84, false);
+        context.drawText(this.textRenderer, Text.literal("02"), rightPageX + pageWidth - 12, 54, 0xFFAE9A84, false);
 
         context.drawWrappedText(
             this.textRenderer,
+            LEFT_PAGE_TEXT,
+            leftPageX,
+            68,
+            pageWidth,
+            0xFF5B4737,
+            false
+        );
+        context.drawCenteredTextWithShadow(this.textRenderer, FORMULA_TEXT, this.backgroundWidth / 2, 90, 0xFF2F2015);
+        context.drawWrappedText(
+            this.textRenderer,
             EXPLANATION_TEXT,
-            24,
-            96,
-            this.backgroundWidth - 48,
-            0xFF4A3828,
+            rightPageX,
+            68,
+            pageWidth,
+            0xFF5B4737,
+            false
+        );
+        context.drawWrappedText(
+            this.textRenderer,
+            RIGHT_PAGE_TEXT,
+            rightPageX,
+            126,
+            pageWidth,
+            0xFF7A6654,
             false
         );
         context.drawWrappedText(
             this.textRenderer,
             FOOTNOTE_TEXT,
-            24,
-            138,
-            this.backgroundWidth - 48,
-            0xFF6C5845,
+            leftPageX,
+            126,
+            pageWidth,
+            0xFF7A6654,
             false
         );
     }
 
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
-        int left = this.x;
-        int top = this.y;
-        int right = left + this.backgroundWidth;
-        int bottom = top + this.backgroundHeight;
-        int centerX = left + this.backgroundWidth / 2;
-
-        // 専用テクスチャを用意するまで、見開きの本のような簡易レイアウトを描画する。
-        context.fillGradient(left, top, right, bottom, 0xFFECD8B4, 0xFFD7BE95);
-        context.fill(centerX - 3, top + 10, centerX + 3, bottom - 10, 0xFF8B6A45);
-        context.fill(left + 8, top + 8, centerX - 5, bottom - 8, 0xFFF7E9C9);
-        context.fill(centerX + 5, top + 8, right - 8, bottom - 8, 0xFFF7E9C9);
-        context.drawBorder(left, top, this.backgroundWidth, this.backgroundHeight, 0xFF6B4A2E);
-        context.drawBorder(left + 8, top + 8, centerX - left - 13, this.backgroundHeight - 16, 0xFFD0B183);
-        context.drawBorder(centerX + 5, top + 8, right - centerX - 13, this.backgroundHeight - 16, 0xFFD0B183);
-        context.fill(left + 16, top + 46, right - 16, top + 47, 0xCCB18A5C);
-        context.fill(left + 16, top + 48, right - 16, top + 49, 0x55FFFFFF);
+        // 背景パネルは render 内で明示的に描画する。
     }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         this.renderBackground(context, mouseX, mouseY, delta);
+        this.drawPanel(context);
         super.render(context, mouseX, mouseY, delta);
     }
 
     @Override
     public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) {
-        // GUI 全体の下地も明るめにして、中央パネルが描けていない場合でも原因を切り分けやすくする。
-        context.fillGradient(0, 0, this.width, this.height, 0xFF5A4332, 0xFF2C2018);
+        this.renderInGameBackground(context);
+        context.fill(0, 0, this.width, this.height, 0x66000000);
+    }
+
+    private void drawPanel(DrawContext context) {
+        int left = this.x;
+        int top = this.y;
+        int right = left + this.backgroundWidth;
+        int bottom = top + this.backgroundHeight;
+        int paperLeft = left + 12;
+        int paperTop = top + 28;
+        int paperRight = right - 12;
+        int paperBottom = bottom - 12;
+        int pageGapLeft = left + this.backgroundWidth / 2 - 3;
+        int pageGapRight = left + this.backgroundWidth / 2 + 3;
+
+        context.fill(left + 3, top + 4, right + 3, bottom + 4, 0x44100000);
+        context.fillGradient(left, top, right, bottom, 0xFFC8B79E, 0xFFB29C80);
+        context.drawBorder(left, top, this.backgroundWidth, this.backgroundHeight, 0xFF5E4A36);
+
+        context.fill(left + 1, top + 1, right - 1, top + 22, 0xFFB49C7D);
+        context.fill(left + 1, top + 23, right - 1, top + 24, 0x88FFF7EA);
+        context.fill(paperLeft, paperTop, paperRight, paperBottom, 0xFFFFFCF4);
+        context.drawBorder(paperLeft, paperTop, paperRight - paperLeft, paperBottom - paperTop, 0xFFDCCFB9);
+        context.fill(pageGapLeft, paperTop + 6, pageGapRight, paperBottom - 6, 0xFFD5C5AB);
+        context.fill(pageGapLeft + 1, paperTop + 6, pageGapRight - 1, paperBottom - 6, 0x55FFF8E8);
+
+        context.fill(paperLeft + 16, paperTop + 58, paperRight - 16, paperTop + 59, 0x33A28C6A);
+        context.fill(paperLeft + 16, paperTop + 60, paperRight - 16, paperTop + 61, 0x22FFFFFF);
+        context.fill(paperLeft + 10, paperTop + 22, pageGapLeft - 10, paperTop + 23, 0x22A28C6A);
+        context.fill(pageGapRight + 10, paperTop + 22, paperRight - 10, paperTop + 23, 0x22A28C6A);
     }
 }
