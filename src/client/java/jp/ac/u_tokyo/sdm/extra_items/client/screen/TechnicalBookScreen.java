df5288eabf74e864adb60ec8ed140189716b9139
miyoshi207 <miyoshi-naoki207@g.ecc.u-tokyo.ac.jp>
2026-03-19 21:25:52 +0900

    透過GUIを作成

--- src/client/java/jp/ac/u_tokyo/sdm/extra_items/client/screen/TechnicalBookScreen.java
+++ src/client/java/jp/ac/u_tokyo/sdm/extra_items/client/screen/TechnicalBookScreen.java
@@ -15,6 +15,11 @@ public final class TechnicalBookScreen extends HandledScreen<TechnicalBookScreen
     private static final Text RIGHT_PAGE_TEXT = Text.translatable("screen.extra_items.technical_book.right_page");
     private static final int PANEL_WIDTH = 292;
     private static final int PANEL_HEIGHT = 180;
+    private static final int PANEL_PADDING = 12;
+    private static final int HEADER_HEIGHT = 24;
+    private static final int PAGE_PADDING = 14;
+    private static final int GUTTER_WIDTH = 8;
+    private static final int FORMULA_SECTION_HEIGHT = 30;
 
     public TechnicalBookScreen(
         TechnicalBookScreenHandler handler,
@@ -36,33 +41,55 @@ public final class TechnicalBookScreen extends HandledScreen<TechnicalBookScreen
     @Override
     protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
         // TODO: ページ送りや章構成を追加する場合は、左右ページごとに操作を分ける。
+        PanelMetrics panel = this.getPanelMetrics();
         int titleWidth = this.textRenderer.getWidth(this.title);
         int subtitleWidth = this.textRenderer.getWidth(SUBTITLE_TEXT);
-        int pageWidth = (this.backgroundWidth - 40) / 2;
-        int leftPageX = 20;
-        int rightPageX = this.backgroundWidth / 2 + 4;
+        int leftPageNumberWidth = this.textRenderer.getWidth("01");
+        int rightPageNumberWidth = this.textRenderer.getWidth("02");
+        int formulaWidth = this.textRenderer.getWidth(FORMULA_TEXT);
+        int lineHeight = this.textRenderer.fontHeight;
 
-        context.drawText(this.textRenderer, this.title, (this.backgroundWidth - titleWidth) / 2, 18, 0xFF4A3221, false);
-        context.drawText(this.textRenderer, SUBTITLE_TEXT, (this.backgroundWidth - subtitleWidth) / 2, 34, 0xFF8C6F58, false);
-        context.drawText(this.textRenderer, Text.literal("01"), leftPageX, 54, 0xFFAE9A84, false);
-        context.drawText(this.textRenderer, Text.literal("02"), rightPageX + pageWidth - 12, 54, 0xFFAE9A84, false);
+        int titleX = panel.left + (panel.width - titleWidth) / 2 - this.x;
+        int titleY = panel.top + 7 - this.y;
+        int subtitleX = panel.paperLeft + (panel.paperWidth - subtitleWidth) / 2 - this.x;
+        int subtitleY = panel.paperTop + 8 - this.y;
+        int formulaX = panel.paperLeft + (panel.paperWidth - formulaWidth) / 2 - this.x;
+        int formulaY = panel.formulaTop + (FORMULA_SECTION_HEIGHT - lineHeight) / 2 - this.y;
+
+        int leftPageX = panel.leftPageLeft + PAGE_PADDING - this.x;
+        int leftPageY = panel.pageBodyTop - this.y;
+        int rightPageX = panel.rightPageLeft + PAGE_PADDING - this.x;
+        int rightPageY = panel.pageBodyTop - this.y;
+        int pageTextWidth = panel.pageWidth - PAGE_PADDING * 2;
+
+        context.drawText(this.textRenderer, this.title, titleX, titleY, 0xFF4A3221, false);
+        context.drawText(this.textRenderer, SUBTITLE_TEXT, subtitleX, subtitleY, 0xFF8C6F58, false);
+        context.drawText(this.textRenderer, "01", leftPageX, panel.pageNumberY - this.y, 0xFFAE9A84, false);
+        context.drawText(
+            this.textRenderer,
+            "02",
+            panel.rightPageRight - PAGE_PADDING - rightPageNumberWidth - this.x,
+            panel.pageNumberY - this.y,
+            0xFFAE9A84,
+            false
+        );
+        context.drawText(this.textRenderer, FORMULA_TEXT, formulaX, formulaY, 0xFF2F2015, false);
 
         context.drawWrappedText(
             this.textRenderer,
             LEFT_PAGE_TEXT,
             leftPageX,
-            68,
-            pageWidth,
+            leftPageY,
+            pageTextWidth,
             0xFF5B4737,
             false
         );
-        context.drawCenteredTextWithShadow(this.textRenderer, FORMULA_TEXT, this.backgroundWidth / 2, 90, 0xFF2F2015);
         context.drawWrappedText(
             this.textRenderer,
             EXPLANATION_TEXT,
             rightPageX,
-            68,
-            pageWidth,
+            rightPageY,
+            pageTextWidth,
             0xFF5B4737,
             false
         );
@@ -70,8 +97,8 @@ public final class TechnicalBookScreen extends HandledScreen<TechnicalBookScreen
             this.textRenderer,
             RIGHT_PAGE_TEXT,
             rightPageX,
-            126,
-            pageWidth,
+            panel.rightPageNoteY - this.y,
+            pageTextWidth,
             0xFF7A6654,
             false
         );
@@ -79,8 +106,8 @@ public final class TechnicalBookScreen extends HandledScreen<TechnicalBookScreen
             this.textRenderer,
             FOOTNOTE_TEXT,
             leftPageX,
-            126,
-            pageWidth,
+            panel.leftPageNoteY - this.y,
+            pageTextWidth,
             0xFF7A6654,
             false
         );
@@ -100,36 +127,96 @@ public final class TechnicalBookScreen extends HandledScreen<TechnicalBookScreen
 
     @Override
     public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderInGameBackground(context);
         context.fill(0, 0, this.width, this.height, 0x66000000);
     }
 
     private void drawPanel(DrawContext context) {
+        PanelMetrics panel = this.getPanelMetrics();
+
+        context.fill(panel.left + 3, panel.top + 4, panel.right + 3, panel.bottom + 4, 0x44100000);
+        context.fillGradient(panel.left, panel.top, panel.right, panel.bottom, 0xFFC8B79E, 0xFFB29C80);
+        context.drawBorder(panel.left, panel.top, panel.width, panel.height, 0xFF5E4A36);
+
+        context.fill(panel.left + 1, panel.top + 1, panel.right - 1, panel.top + HEADER_HEIGHT, 0xFFB49C7D);
+        context.fill(panel.left + 1, panel.top + HEADER_HEIGHT, panel.right - 1, panel.top + HEADER_HEIGHT + 1, 0x88FFF7EA);
+
+        context.fill(panel.paperLeft, panel.paperTop, panel.paperRight, panel.paperBottom, 0xFFFFFCF4);
+        context.drawBorder(panel.paperLeft, panel.paperTop, panel.paperWidth, panel.paperHeight, 0xFFDCCFB9);
+
+        context.fill(panel.gutterLeft, panel.paperTop + 6, panel.gutterRight, panel.paperBottom - 6, 0xFFD5C5AB);
+        context.fill(panel.gutterLeft + 1, panel.paperTop + 6, panel.gutterRight - 1, panel.paperBottom - 6, 0x55FFF8E8);
+
+        context.fill(panel.paperLeft + 12, panel.formulaTop - 6, panel.paperRight - 12, panel.formulaTop - 5, 0x22A28C6A);
+        context.fill(panel.paperLeft + 12, panel.formulaBottom + 5, panel.paperRight - 12, panel.formulaBottom + 6, 0x22A28C6A);
+        context.fill(panel.paperLeft + 18, panel.leftPageNoteY - 8, panel.gutterLeft - 12, panel.leftPageNoteY - 7, 0x22A28C6A);
+        context.fill(panel.gutterRight + 12, panel.rightPageNoteY - 8, panel.paperRight - 18, panel.rightPageNoteY - 7, 0x22A28C6A);
+    }
+
+    private PanelMetrics getPanelMetrics() {
         int left = this.x;
         int top = this.y;
-        int right = left + this.backgroundWidth;
-        int bottom = top + this.backgroundHeight;
-        int paperLeft = left + 12;
-        int paperTop = top + 28;
-        int paperRight = right - 12;
-        int paperBottom = bottom - 12;
-        int pageGapLeft = left + this.backgroundWidth / 2 - 3;
-        int pageGapRight = left + this.backgroundWidth / 2 + 3;
+        int width = this.backgroundWidth;
+        int height = this.backgroundHeight;
+        int right = left + width;
+        int bottom = top + height;
 
-        context.fill(left + 3, top + 4, right + 3, bottom + 4, 0x44100000);
-        context.fillGradient(left, top, right, bottom, 0xFFC8B79E, 0xFFB29C80);
-        context.drawBorder(left, top, this.backgroundWidth, this.backgroundHeight, 0xFF5E4A36);
+        int paperLeft = left + PANEL_PADDING;
+        int paperTop = top + HEADER_HEIGHT + 4;
+        int paperRight = right - PANEL_PADDING;
+        int paperBottom = bottom - PANEL_PADDING;
+        int paperWidth = paperRight - paperLeft;
+        int paperHeight = paperBottom - paperTop;
 
-        context.fill(left + 1, top + 1, right - 1, top + 22, 0xFFB49C7D);
-        context.fill(left + 1, top + 23, right - 1, top + 24, 0x88FFF7EA);
-        context.fill(paperLeft, paperTop, paperRight, paperBottom, 0xFFFFFCF4);
-        context.drawBorder(paperLeft, paperTop, paperRight - paperLeft, paperBottom - paperTop, 0xFFDCCFB9);
-        context.fill(pageGapLeft, paperTop + 6, pageGapRight, paperBottom - 6, 0xFFD5C5AB);
-        context.fill(pageGapLeft + 1, paperTop + 6, pageGapRight - 1, paperBottom - 6, 0x55FFF8E8);
+        int gutterLeft = left + width / 2 - GUTTER_WIDTH / 2;
+        int gutterRight = gutterLeft + GUTTER_WIDTH;
+        int pageWidth = (paperWidth - GUTTER_WIDTH) / 2;
+        int leftPageLeft = paperLeft;
+        int leftPageRight = leftPageLeft + pageWidth;
+        int rightPageLeft = gutterRight;
+        int rightPageRight = paperRight;
 
-        context.fill(paperLeft + 16, paperTop + 58, paperRight - 16, paperTop + 59, 0x33A28C6A);
-        context.fill(paperLeft + 16, paperTop + 60, paperRight - 16, paperTop + 61, 0x22FFFFFF);
-        context.fill(paperLeft + 10, paperTop + 22, pageGapLeft - 10, paperTop + 23, 0x22A28C6A);
-        context.fill(pageGapRight + 10, paperTop + 22, paperRight - 10, paperTop + 23, 0x22A28C6A);
+        int formulaTop = paperTop + 26;
+        int formulaBottom = formulaTop + FORMULA_SECTION_HEIGHT;
+        int pageNumberY = formulaBottom + 8;
+        int pageBodyTop = pageNumberY + this.textRenderer.fontHeight + 8;
+        int leftPageNoteY = paperBottom - 58;
+        int rightPageNoteY = paperBottom - 58;
+
+        return new PanelMetrics(
+            left, top, right, bottom, width, height,
+            paperLeft, paperTop, paperRight, paperBottom, paperWidth, paperHeight,
+            gutterLeft, gutterRight,
+            leftPageLeft, leftPageRight, rightPageLeft, rightPageRight, pageWidth,
+            formulaTop, formulaBottom, pageNumberY, pageBodyTop, leftPageNoteY, rightPageNoteY
+        );
+    }
+
+    private record PanelMetrics(
+        int left,
+        int top,
+        int right,
+        int bottom,
+        int width,
+        int height,
+        int paperLeft,
+        int paperTop,
+        int paperRight,
+        int paperBottom,
+        int paperWidth,
+        int paperHeight,
+        int gutterLeft,
+        int gutterRight,
+        int leftPageLeft,
+        int leftPageRight,
+        int rightPageLeft,
+        int rightPageRight,
+        int pageWidth,
+        int formulaTop,
+        int formulaBottom,
+        int pageNumberY,
+        int pageBodyTop,
+        int leftPageNoteY,
+        int rightPageNoteY
+    ) {
     }
 }
