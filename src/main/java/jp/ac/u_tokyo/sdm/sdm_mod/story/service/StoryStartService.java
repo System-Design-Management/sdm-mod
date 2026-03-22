cb2b2eb0814fc04da68e0d9e219b070188a0bb0a
miyoshi207 <miyoshi-naoki207@g.ecc.u-tokyo.ac.jp>
2026-03-22 18:47:16 +0900

    複数アイテム与えるように修正

--- src/main/java/jp/ac/u_tokyo/sdm/sdm_mod/story/service/StoryStartService.java
+++ src/main/java/jp/ac/u_tokyo/sdm/sdm_mod/story/service/StoryStartService.java
@@ -20,6 +20,10 @@ public final class StoryStartService {
         server.getPlayerManager().getPlayerList().forEach(player -> {
             executePlayerCommand(server, player, "function thepa:give/revolver");
             executePlayerCommand(server, player, "function thepa:give/bullets");
+            executePlayerCommand(server, player, "function thepa:give/pump_shotgun");
+            executePlayerCommand(server, player, "function thepa:give/shells");
+            executePlayerCommand(server, player, "function thepa:give/m1_garand");
+            executePlayerCommand(server, player, "function thepa:give/medium_round");
         });
 
         StoryManager storyManager = StoryModule.getStoryManager();
