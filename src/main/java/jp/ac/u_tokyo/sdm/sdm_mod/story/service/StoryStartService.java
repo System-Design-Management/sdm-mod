f5b777180c26785ceb9cfa7921a2cab42442efc7
miyoshi207 <miyoshi-naoki207@g.ecc.u-tokyo.ac.jp>
2026-03-22 18:24:27 +0900

    初期化処理を追加

--- src/main/java/jp/ac/u_tokyo/sdm/sdm_mod/story/service/StoryStartService.java
+++ src/main/java/jp/ac/u_tokyo/sdm/sdm_mod/story/service/StoryStartService.java
@@ -5,6 +5,9 @@ import jp.ac.u_tokyo.sdm.sdm_mod.story.StoryModule;
 import jp.ac.u_tokyo.sdm.sdm_mod.story.runtime.StoryManager;
 import jp.ac.u_tokyo.sdm.sdm_mod.story.state.StoryProgress;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameMode;
 
 public final class StoryStartService {
     private StoryStartService() {
@@ -12,9 +15,42 @@ public final class StoryStartService {
 
     public static StoryProgress start(MinecraftServer server) {
         GameRulesInitializer.applyStoryDefaults(server);
+        server.getPlayerManager().getPlayerList().forEach(StoryStartService::resetPlayerState);
+        server.getPlayerManager().getPlayerList().forEach(StoryStartService::preparePlayerForStory);
+        executeServerCommand(server, "function thepa:give/revolver with @a");
+        executeServerCommand(server, "function thepa:give/bullets with @a");
 
         StoryManager storyManager = StoryModule.getStoryManager();
         storyManager.reset();
         return storyManager.getProgress();
+    }
+
+    private static void resetPlayerState(ServerPlayerEntity player) {
+        player.getInventory().clear();
+        player.getEnderChestInventory().clear();
+        player.clearStatusEffects();
+        player.extinguish();
+        player.setOnGround(true);
+        player.setHealth(player.getMaxHealth());
+        player.getHungerManager().setFoodLevel(20);
+        player.getHungerManager().setSaturationLevel(5.0f);
+        player.setAir(player.getMaxAir());
+        player.setFireTicks(0);
+        player.fallDistance = 0.0f;
+        player.setFrozenTicks(0);
+        player.experienceLevel = 0;
+        player.totalExperience = 0;
+        player.experienceProgress = 0.0f;
+    }
+
+    private static void preparePlayerForStory(ServerPlayerEntity player) {
+        player.changeGameMode(GameMode.ADVENTURE);
+
+        // TODO: ストーリー開始地点の座標が確定したら、ここでテレポートする。
+    }
+
+    private static void executeServerCommand(MinecraftServer server, String command) {
+        ServerCommandSource commandSource = server.getCommandSource().withLevel(2);
+        server.getCommandManager().executeWithPrefix(commandSource, command);
     }
 }
