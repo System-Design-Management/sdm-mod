21d16757328dd0ad590d6159fa6f81c40b93bc44
raito <raito-1206-33@g.ecc.u-tokyo.ac.jp>
2026-05-13 17:19:20 +0900

    feat: search_pcブロックに向き対応を追加

--- src/main/java/jp/ac/u_tokyo/sdm/sdm_mod/block/SearchPcBlock.java
+++ src/main/java/jp/ac/u_tokyo/sdm/sdm_mod/block/SearchPcBlock.java
@@ -2,12 +2,30 @@ package jp.ac.u_tokyo.sdm.sdm_mod.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 
 public class SearchPcBlock extends Block {
+    public static final EnumProperty<Direction> FACING = Properties.HORIZONTAL_FACING;
+
     public SearchPcBlock(Settings settings) {
         super(settings);
+        setDefaultState(getStateManager().getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return getDefaultState().with(FACING, context.getHorizontalPlayerFacing().getOpposite());
     }
 
     @Override
